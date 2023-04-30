7774704d06584c109787b3f3134a56a65776593c
Luis Leal <luisrrleal@gmail.com>
2023-04-29 19:41:38 -0600

    Add new views and controllers

--- src/main/java/com/example/proyectofinalsemestre4topicosavanzadosdeprogramacion/HelloApplication.java
+++ src/main/java/com/example/proyectofinalsemestre4topicosavanzadosdeprogramacion/HelloApplication.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("solicitud-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1100, 600);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles/index.css")).toString());
         stage.setTitle("MoveItNow!");
---
+++ src/main/java/com/example/proyectofinalsemestre4topicosavanzadosdeprogramacion/controllers/SolicitudController.java
@@ -0,0 +1,5 @@
+package com.example.proyectofinalsemestre4topicosavanzadosdeprogramacion.controllers;
+
+public class SolicitudController {
+
+}
